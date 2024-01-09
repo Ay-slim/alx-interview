10d6c7a6a045bf8c818b0a876b984ee78540800f
Ay-slim <ayooluwaadedipe@gmail.com>
2024-01-09 09:23:17 +0000

    Pycodestyle fixes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,31 +6,34 @@ Lockboxes module
 
 def canUnlockAll(boxes):
     """
-    A function that checks if all boxes can be unlocked with the contained keys
-    @boxes: A list of lists with each list containing keys to unlock corresponding indexes
+    A function that checks if all boxes can be unlocked with the provided keys
+    @boxes: A list of lists with each list containing keys to unlock
+      corresponding indexes
     Return: A boolean indicating whether or not all boxes can be unlocked
     """
-    #[[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
-    locked_boxes = [x for x in range(0, len(boxes))] #boxes that have not been opened
-    unused_keys = set([0]) #unique set of keys that have not been checked
-    current_key = 0 #current box being checked
-    while(len(unused_keys) and len(locked_boxes)):
-      current_key = next(iter(unused_keys)) #Pick a random unused key
-      unused_keys.remove(current_key) #Remove it from unused keys
-      while(current_key != None):
-        if (len(locked_boxes) == 0):
-          return True
-        current_key_box = boxes[current_key]
-        if current_key in locked_boxes:
-          locked_boxes.remove(current_key)
-        new_current_key = None
-        for each_key in current_key_box:
-          if (each_key in locked_boxes):
-            if (not new_current_key):
-              new_current_key = each_key
+    locked_boxes = [x for x in range(0, len(boxes))]  # unopened boxes
+    unused_keys = set([0])  # unique set of keys that have not been checked
+    current_key = 0   # current box being checked
+    while len(unused_keys) and len(locked_boxes):
+        current_key = next(iter(unused_keys))   # Pick a random unused key
+        unused_keys.remove(current_key)   # Remove it from unused keys
+        while current_key is not None:
+            if len(locked_boxes) == 0:
+                return True
+            current_key_box = boxes[current_key]
+            if current_key in locked_boxes:
+                locked_boxes.remove(current_key)
             else:
-              unused_keys.add(each_key)
-        current_key = new_current_key
-    if (len(locked_boxes)):
-      return False
+                # Opened box, break and get new key from outer loop
+                break
+            new_current_key = None
+            for each_key in current_key_box:
+                if each_key in locked_boxes:
+                    if not new_current_key:
+                        new_current_key = each_key
+                    else:
+                        unused_keys.add(each_key)
+            current_key = new_current_key
+    if len(locked_boxes):
+        return False
     return True
